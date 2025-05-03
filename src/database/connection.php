eb47aee968ae5da89365f8c6cc6de4f0f67284b0
GerodiazRogel-BSIT32E1 <99164720+Rogel0@users.noreply.github.com>
2025-05-04 02:49:37 +0800

    updating database

--- src/database/connection.php
+++ src/database/connection.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "try";
+$dbname = "po_database";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
